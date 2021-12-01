dd3c2cd725f169466aba7d591d39b2e03bab0872
Bui Minh Quan <buiminhquan.hcmus@gmail.com>
2021-12-01 15:15:19 +0700

    Fix KhoaDaoTao table name.

---
+++ DctAPI/Migrations/20211201081436_Update5.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DctAPI.Migrations
+{
+    public partial class Update5 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_KhoaDaoTaoEntity",
+                table: "KhoaDaoTaoEntity");
+
+            migrationBuilder.RenameTable(
+                name: "KhoaDaoTaoEntity",
+                newName: "KhoaDaoTao");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_KhoaDaoTao",
+                table: "KhoaDaoTao",
+                column: "ID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_KhoaDaoTao",
+                table: "KhoaDaoTao");
+
+            migrationBuilder.RenameTable(
+                name: "KhoaDaoTao",
+                newName: "KhoaDaoTaoEntity");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_KhoaDaoTaoEntity",
+                table: "KhoaDaoTaoEntity",
+                column: "ID");
+        }
+    }
+}
