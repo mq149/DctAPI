db05305d9ec8d714e5fbe82aedf72ddc819eb75a
HoangQuan0302 <quanminhaa@gmail.com>
2021-12-26 19:53:10 +0700

    Add Authentication

---
+++ DctAPI/Controllers/AdminRegistrationRightsController.cs
@@ -0,0 +1,84 @@
+﻿using DctApi.Shared.Enums;
+using DctApi.Shared.Models;
+using DctAPI.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.JsonWebTokens;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DctAPI.Controllers {
+    [Authorize(Roles=RoleName.Admin)]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AdminRegistrationRightsController : ControllerBase {
+        private readonly UserManager<UserEntity> _userManage;
+        private readonly RoleManager<RoleEntity> _roleManage;
+        private readonly IConfiguration _configuration;
+
+        public AdminRegistrationRightsController(UserManager<UserEntity> userManager, RoleManager<RoleEntity> roleManage, IConfiguration configuration) {
+            _userManage = userManager;
+            _roleManage = roleManage;
+            _configuration = configuration;
+        }
+
+        [HttpPost]
+        [Route("RegisterAdmin")]
+        public async Task<IActionResult> Register([FromBody] RegisteModel model) {
+            var userExits = await _userManage.FindByNameAsync(model.username);
+            if (userExits != null) {
+                return StatusCode(StatusCodes.Status500InternalServerError, new Response { status = "Error", message = "User already exits" });
+            }
+            UserEntity userNew = new UserEntity() {
+                UserName = model.username,
+                SDT = model.username,
+                Email = model.email,
+            };
+            var result = await _userManage.CreateAsync(userNew, model.password);
+            if (!result.Succeeded) {
+                return StatusCode(StatusCodes.Status500InternalServerError, new Response { status = "Error", message = "Create user faild" });
+            }
+            //set role for user
+            if (!await _roleManage.RoleExistsAsync(RoleName.Admin)) {
+                await _roleManage.CreateAsync(new RoleEntity() { Id = (int)Role.Admin, Name = "Admin", Ten = "Admin" });
+            }
+            await _userManage.AddToRoleAsync(userNew, RoleName.Admin);
+            return Ok(new Response { status = "Success", message = "User created successfully" });
+
+        }
+
+        [HttpPost]
+        [Route("RegisterShipper")]
+        public async Task<IActionResult> RegisterShipper([FromBody] RegisteModel model) {
+            var userExits = await _userManage.FindByNameAsync(model.username);
+            if (userExits != null) {
+                return StatusCode(StatusCodes.Status500InternalServerError, new Response { status = "Error", message = "User already exits" });
+            }
+            UserEntity userNew = new UserEntity() {
+                UserName = model.username,
+                SDT = model.username,
+                Email = model.email,
+            };
+            var result = await _userManage.CreateAsync(userNew, model.password);
+            if (!result.Succeeded) {
+                return StatusCode(StatusCodes.Status500InternalServerError, new Response { status = "Error", message = "Create user faild" });
+            }
+            //set role for user
+            if (!await _roleManage.RoleExistsAsync(RoleName.Shipper)) {
+                await _roleManage.CreateAsync(new RoleEntity() { Id = (int)Role.Shipper, Name = "Shipper", Ten = "Shipper" });
+            }
+            await _userManage.AddToRoleAsync(userNew, RoleName.Shipper);
+            return Ok(new Response { status = "Success", message = "User created successfully" });
+
+        }
+    }
+}
